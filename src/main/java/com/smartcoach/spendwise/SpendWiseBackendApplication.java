a3b6ce7123b25cfd175e41096a84ee6306b9e9e7
enockTheGeek <ikhavienock6@gmail.com>
2026-01-09 11:24:01 +0300

    First commit

---
+++ src/main/java/com/smartcoach/spendwise/SpendWiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartcoach.spendwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendWiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendWiseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcoach/spendwise/SpendWiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcoach.spendwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendWiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
